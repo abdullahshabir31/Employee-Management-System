e30db1ff1990bca57dd98fe96bd9b90702d96822
Abdullah Shabir <abdullahshabir31@gmail.com>
2026-09-01 19:32:09 +0500

    Update main application configuration

--- Backend/main.py
+++ Backend/main.py
@@ -5,13 +5,16 @@ from routers.departments import router as departments_router
 from routers.employees import router as employees_router
 from routers.projects import router as projects_router
 from routers.employee_projects import router as employee_project_router
+from routers.auth import router as auth_router
 
 
 app = FastAPI()
+app.include_router(auth_router)
 app.include_router(departments_router)
 app.include_router(employees_router)
 app.include_router(projects_router)
 app.include_router(employee_project_router)
+
 
 @app.get("/")
 def root():
